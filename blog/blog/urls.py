c168ff16b90a8b1fd3ae25795bc402807c8874b2
mycodingx <mycodingx@gmail.com>
2019-07-15 12:58:33 +0530

    Adding Tempaltes and Static Resources

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from home import views
 urlpatterns = [
     path('',views.index,name="index"),
+    path('home/',include('home.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- blog/home/views.py
+++ blog/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+"""
 def index(request):
     return HttpResponse("Hello World!")
+"""
+
+def index(request):
+    data_dict = {'insert_data':"Hello from Django template tag"}
+    return render(request,'home/index.html',context=data_dict)
+
